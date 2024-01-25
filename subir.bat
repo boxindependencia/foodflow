cf13c5fd5e5d4d0b2f6e6b81aa4da486c98e4649
Alis Ruben Cerna <alis.cerna@correoaiep.cl>
2024-01-25 16:08:10 -0300

    actualizando ubicacion

--- subir.bat
+++ subir.bat
@@ -4,8 +4,8 @@ call ng build --base-href /foodflow/
 echo "copiando archivos finales a carpeta docs"
 del docs\* /q /s
 echo "continuamos...."
-move dist\browser\* docs
-pasue
+move dist\foodflow\browser\* docs
+pause
 git add .
 git commit -m %1
 git push
